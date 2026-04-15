9d0d464e1be9f2b4b5faf336c8e1c9484640dbce
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-04-15 14:52:26 -0300

    [feat]: adicionar e configurar os Web Components do GovBR

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@govbr-ds/core/dist/core-tokens.min.css'
+import '@govbr-ds/core/dist/core.min.css'
 
+import { defineCustomElements } from '@govbr-ds/webcomponents/dist/loader'
 const app = createApp(App)
 
 app.use(router)
+defineCustomElements()
 
 app.mount('#app')
--- src/views/PaginaInicial.vue
+++ src/views/PaginaInicial.vue
@@ -1,36 +1,11 @@
 <template>
-  <div class="container">
-    <h1>{{ i }}</h1>
-    <button @click="addCont">Adicionar contador</button>
-  </div>
+  <h1>Olá {{ name }}</h1>
+  <br-button emphasis="primary">Clique aqui</br-button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-
-const i = ref(0)
-const addCont = () => i.value++
-
+const name = ref('Jon Doe')
 </script>
 
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  h1 {
-    font-size: 5rem;
-  }
-  button {
-    padding: 1rem 2rem;
-    border: 0;
-    border-radius: 25px;
-    transition: 0.4s;
-    cursor: pointer;
-    &:hover {
-      background-color: #d3d3d3;
-    }
-  }
-}
-</style>
+<style scoped></style>
