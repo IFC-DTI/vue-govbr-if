f008a156fda34c6cb68a520d08374f2bb9dcfd71
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-04-22 14:54:19 -0300

    [test]: realizar teste de funcionamento de view

--- src/views/PaginaInicial.vue
+++ src/views/PaginaInicial.vue
@@ -1,11 +1,29 @@
 <template>
-  <h1>Olá {{ name }}</h1>
-  <br-button emphasis="primary">Clique aqui</br-button>
+  <div style="width: 50%; display: block; margin: 0 auto">
+    <br-input
+      @valueChange="mudaNome"
+      label="Escreva seu nome: "
+      placeholder="Seu nome aqui."
+      density="large"
+    ></br-input>
+
+    <h1>Olá {{ nome }}</h1>
+  </div>
+  <div style="display: flex;
+  flex-direction: column; align-items: center; justify-content: space-between;">
+    <br-button @click="() => contador++" shape="block">Clique aqui</br-button>
+    <h2>{{ contador }}</h2>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const name = ref('Jon Doe')
+
+const mudaNome = (event: CustomEvent<string>): void => {
+  nome.value = event.detail
+}
+const nome = ref('')
+const contador = ref(0)
 </script>
 
 <style scoped></style>
