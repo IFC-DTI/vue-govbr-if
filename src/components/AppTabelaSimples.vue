e767cd71bc6f1bc321a1d74dd85550baf2094eab
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-05-06 13:36:09 -0300

    [feat] Adicionar exemplo de tabela simples usando web components

--- src/components/AppTabelaSimples.vue
+++ src/components/AppTabelaSimples.vue
@@ -1,9 +1,10 @@
 <template>
-  <div :title="title">
+  <h3>Feito Manualmente</h3>
+  <div>
     <table>
       <thead>
         <tr>
-          <th v-for="header in headers" :key="header.key" scope="col">
+          <th v-for="header in headers" :key="header.key">
             {{ header.label }}
           </th>
         </tr>
@@ -17,6 +18,28 @@
       </tbody>
     </table>
   </div>
+
+  <br />
+  <br />
+
+  <h3>Feito com Web Components</h3>
+  <br-table has-row-divider column-width="fill" overflow="truncate">
+    <br-table-header slot="header">
+      <br-table-row>
+        <br-table-header-cell v-for="header in headers" :key="header.key" column-width="fill">
+          {{ header.label }}
+        </br-table-header-cell>
+      </br-table-row>
+    </br-table-header>
+
+    <br-table-body slot="body">
+      <br-table-row v-for="(item, i) in data" :key="i">
+        <br-table-cell v-for="header in headers" :key="header.key" column-width="fill">
+          {{ item[header.key] }}
+        </br-table-cell>
+      </br-table-row>
+    </br-table-body>
+  </br-table>
 </template>
 
 <script setup lang="ts" generic="T extends Record<string, any>">
